f37232db6ce20dbe119ef73ae345f208660c0dad
RamyaS-30 <ramyasundar030@gmail.com>
2025-11-17 21:55:16 +0530

    Update ForgotPassword.jsx

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -15,7 +15,7 @@ export default function ForgotPassword() {
     setLoading(true);
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/forgot-password', {
+      const res = await axios.post('https://flexopted-backend.onrender.com/api/auth/forgot-password', {
         email,
       });
 
