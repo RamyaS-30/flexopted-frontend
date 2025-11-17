02909788fc20a6284a5dd30c9d609f401ec68f2b
RamyaS-30 <ramyasundar030@gmail.com>
2025-11-17 21:35:26 +0530

    Update ResetPassword.jsx

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -22,7 +22,7 @@ export default function ResetPassword() {
 
     try {
       const res = await axios.post(
-        `http://localhost:5000/api/auth/reset-password/${token}`,
+        `https://flexopted-backend.onrender.com/api/auth/reset-password/${token}`,
         { password }
       );
 
