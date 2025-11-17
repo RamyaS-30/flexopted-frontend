789d3c79850aac15e4ec784347686c5f2b035958
RamyaS-30 <ramyasundar030@gmail.com>
2025-11-17 21:40:16 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     setError(null);
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('https://flexopted-backend.onrender.com/api/auth/login', { email, password });
       login(res.data.user, res.data.token);
       navigate('/');
     } catch (err) {
